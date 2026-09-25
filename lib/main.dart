a0d2e33e5fd341fc5b56354dfe59bde14ee7ec70
El Fahreza Sufi <el.fahrezasufi@gmail.com>
2026-09-25 22:57:23 +0700

    feat: update app title and theme

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'AnimeVerse',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        fontFamily: 'Urbanist',
       ),
       home: const SignInScreen(),
       debugShowCheckedModeBanner: false,
